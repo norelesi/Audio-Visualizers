050f0a37db2fd84546ff65f29ddd2fb2f5ed0125
norelesi <116086948+norelesi@users.noreply.github.com>
2026-04-19 14:32:04 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cyber-vis-v1';
+const ASSETS = [
+  './index.html',
+  './Vis7.html',
+  './x3.html',
+  './Vis2HD.html',
+  './Vis3.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
